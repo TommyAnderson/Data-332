c6ecda93daa6647d775cde8865e96086af621d27
Tommy Anderson <90470608+TommyAnderson@users.noreply.github.com>
2025-02-26 15:22:54 -0600

    Update manyTruckingData.R

--- manyTruckingData.R
+++ manyTruckingData.R
@@ -1,8 +1,3 @@
-# get all the shit
-# for the truckers multiple
-# answer all questions from other one
-# show trucker pay in bar?? chart
-
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
@@ -23,20 +18,25 @@ dfTruck1796 <- read_excel("TruckerData/truck data 1769.xlsx", sheet = 2, skip = 
 dfPay <- read_excel("TruckerData/Driver Pay Sheet.xlsx",.name_repair = 'universal')
 
 
-df <- rbind(dfTruck0001,dfTruck0369,dfTruck1226,dfTruck1442,
+dfTrucks <- rbind(dfTruck0001,dfTruck0369,dfTruck1226,dfTruck1442,
             dfTruck1478,dfTruck1539,dfTruck1796)
 
-dfStartingPivot <- df %>%
+dfLocations <- dfTrucks %>%
   group_by(Truck.ID) %>%
-  summarize(count = n())
+  summarize(unique_locations = n_distinct(c(Starting.Location, Delivery.Location)))
 
-dfTotalMilesDriven <- df %>%
+dfTotalMilesDriven <- dfTrucks %>%
   group_by(Truck.ID) %>%
-  summarize(totalMiles = sum(Odometer.Ending - Odometer.Beginning))
+  summarize(Total_Miles = sum(Odometer.Ending - Odometer.Beginning))
+
+dfMerged <- dfTotalMilesDriven %>%
+  inner_join(dfPay, by = "Truck.ID") %>%
+  mutate(Total_Pay = labor_per_mil * Total_Miles)
 
 
-dfTotalPayDriver <- dfTotalMilesDriven %>%
-  inner_join(dfPay, by = c("Truck.ID" = "Truck.ID")) %>%
-  mutate(totalPay = labor_per_mil * totalMiles)
-
+ggplot(dfMerged, aes(x = reorder(Truck.ID, Total_Pay), y = Total_Pay, fill = factor(Truck.ID))) +
+  geom_bar(stat = "identity") +
+  labs(title = "Total Pay Per Trucker", x = "Truck ID", y = "Total Pay ($)") +
+  theme_minimal() +
+  theme(legend.position = "none")
 
