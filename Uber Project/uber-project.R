21bfcc5f51b788a1d4f081850ee80afcbcf397b1
Tommy Anderson <tommyanderson21@augustana.edu>
2025-05-01 20:54:37 -0500

    Added Files

---
+++ Uber Project/uber-project.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(DT)
+library(rsconnect)
+library(readxl)
+library(RCurl)
+library(lubridate)
+library(janitor)
+library(purrr)
+
+rm(list = ls())
+setwd("C:/Users/tomma/Data332/Uber Project")
+
+
